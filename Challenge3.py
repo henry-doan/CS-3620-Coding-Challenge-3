4ef05fbe6ff6adf5ef2df664dd12b56c56849b33
Henry Doan <henrydoan96@gmail.com>
2021-09-18 14:13:39 -0600

    added comments

--- Challenge3.py
+++ Challenge3.py
@@ -7,25 +7,29 @@ def moreDiscount(amt):
   discounted = 0.05 * amt 
   return amt - discounted
 
-# print(moreDiscount(studentDiscount(90.00)))
+# test case with $90
+print(moreDiscount(studentDiscount(90.00)))
 
 # Part 2
-#x*(x+5)^2 where x= 5 with order of operations
+#x*(x+5)^2 where x = 5 with order of operations
 result = (lambda x:(x*((x+5)**2)))(5)
-# print(result)
+print(result)
 
 # Part 3
+# initial prices
 prices = [10.00, 20.00, 30.00, 40.00, 50.00]
 
 def discountAll(price):
   discounted = 0.10 * price 
   return price - discounted
 
+# map and discount the list
 result = list(map(discountAll, prices))
 
-# print(result)
+print(result)
 
 # Part 4
+# computer parent class
 class Computer:
 
   def __init__(self, cpu, ram, storage, graphicCard):
@@ -46,6 +50,7 @@ class Computer:
     print(self.storage)
     print(self.graphicCard)
 
+# desktop child class inherit parent class
 class Desktop(Computer):
 
   def __init__(self, powerWatts):
@@ -57,6 +62,7 @@ class Desktop(Computer):
   def displayDesktopSpecs(self):
     print(self.powerWatts)
 
+# desktop child class inherit parent class
 class Laptop(Computer):
 
   def __init__(self, batteryLife):
@@ -68,24 +74,24 @@ class Laptop(Computer):
   def displayLaptopSpecs(self):
     print(self.batteryLife)
 
-# com = Computer("", "", "", "")
+com = Computer("", "", "", "")
+# would get the user input on each field by autofill to show results
+com.getspecs("Intel I7", "16GB", "100TB", "3090")
+com.displayspecs()
 
-# com.getspecs("Intel I7", "16GB", "100TB", "3090")
-# com.displayspecs()
+desktop1 = Desktop("")
+desktop1.getspecs("Intel I9", "32GB", "10TB", "30800")
 
-# desktop1 = Desktop("")
-# desktop1.getspecs("Intel I9", "32GB", "10TB", "30800")
+desktop1.displayspecs()
+desktop1.getDesktopSpecs("1000Watts")
+desktop1.displayDesktopSpecs()
 
-# desktop1.displayspecs()
-# desktop1.getDesktopSpecs("1000Watts")
-# desktop1.displayDesktopSpecs()
+laptop1 = Laptop("")
+laptop1.getspecs("Intel I7", "16GB", "100TB", "3090")
 
-# laptop1 = Laptop("")
-# laptop1.getspecs("Intel I7", "16GB", "100TB", "3090")
-
-# laptop1.displayspecs()
-# laptop1.getLaptopSpecs("700hrs")
-# laptop1.displayLaptopSpecs()
+laptop1.displayspecs()
+laptop1.getLaptopSpecs("700hrs")
+laptop1.displayLaptopSpecs()
 
 # Part 5
 class OverloadingMult: 
